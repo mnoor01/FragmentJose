b45cb690c219c08958487b43d8e09798a99a9dd7
mnoor01 <melinag1992@gmail.com>
2017-11-19 16:26:10 -0500

    initial commit

---
+++ app/src/main/java/com/example/c4q/inclassjose/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c4q.inclassjose;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {// always do work with a glass of water by your side it will never leave you.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
